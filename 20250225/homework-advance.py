77d354970d95dbaa9e686b144ce41ea56bb22d05
Young <young20050727@gmail.com>
2025-02-25 10:55:13 +0800

    20250225 done

--- 20250225/homework-advance.py
+++ 20250225/homework-advance.py
@@ -1,6 +1,11 @@
+import json
+
+with open("20250225/age.json", "r") as file:
+    data = json.load(file)
+
 age = int(input("請輸入你的年齡: "))
 
-if age >= 20:
+if age >= data["Taiwan"]:
     print("你有投票權")
 else:
     print("你沒有投票權")
