b1e90858d9421eb744c8d6c86f0aba2bee90afe1
Young <young20050727@gmail.com>
2025-03-25 11:23:10 +0800

    20250325-2-2 fix

--- src/20250325/0325_2-2_B1243030.py
+++ src/20250325/0325_2-2_B1243030.py
@@ -16,7 +16,22 @@ def grade(score):
     else:
         return 'F'
 
+def grade_score(grade):
+    if grade == 'A':
+        return "90~100"
+    elif grade == 'B+':
+        return "85~89"
+    elif grade == 'B':
+        return "80~84"
+    elif grade == 'C+':
+        return "70~79"
+    elif grade == 'C':
+        return "60~69"
+    else:
+        return "0~59"
+
 if __name__ == "__main__":
     score = input("輸入分數：")
     print(f"等第：{grade(int(score))}")
     print("及格" if (grade(int(score)) != 'F' and grade(int(score)) != 'N') else "不及格")
+    print(f"分數區間在 {grade_score(grade(int(score)))}")
