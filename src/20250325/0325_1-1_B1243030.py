57ac8a160d9078d88fd64510e767fc86b12720ec
Young <young20050727@gmail.com>
2025-03-25 10:30:06 +0800

    20250325-1

---
+++ src/20250325/0325_1-1_B1243030.py
@@ -0,0 +1,15 @@
+from rich import print
+
+# 閏年
+def is_leap_year(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+def is_valid_year(year):
+    return year >= 1
+
+if __name__ == "__main__":
+    print("輸入年份：")
+    year = int(input())
+    while(not is_valid_year(year)):
+        year = int(input())
+    print(f"{year} 年{'是' if is_leap_year(year) else '不是'}閏年")
