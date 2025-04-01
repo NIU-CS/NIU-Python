545539595090f9e9141906c58867bc6f387adbe2
Young <young20050727@gmail.com>
2025-04-01 11:21:13 +0800

    20250401_2

--- src/20250401/0401_2-1_B1243030.py
+++ src/20250401/0401_2-1_B1243030.py
@@ -1,3 +1,8 @@
 from rich import print
 
 if __name__ == "__main__":
+    numbers = input("input 3 numbers: ").split()
+    numbers = list(map(int, numbers))
+    targets = [numbers[0], numbers[0] + numbers[1] * numbers[2], numbers[1]]
+    for i in range(targets[0], targets[1], targets[2]):
+        print(i)
--- src/20250401/0401_2-2_B1243030.py
+++ src/20250401/0401_2-2_B1243030.py
@@ -1,3 +1,10 @@
 from rich import print
 
 if __name__ == "__main__":
+    fib = [1, 1]
+    target = int(input("input a number: "))
+    for i in range(2, target):
+        fib.append(fib[i - 1] + fib[i - 2])
+
+    for i in range(target):
+        print(f"number {i + 1}: {fib[i]}")
