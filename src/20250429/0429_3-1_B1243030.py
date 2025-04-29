c2653b72817bd8a1bbbb5a2727278110952dc6eb
Young <young20050727@gmail.com>
2025-04-29 11:57:55 +0800

    fix: 0429_3-1

--- src/20250429/0429_3-1_B1243030.py
+++ src/20250429/0429_3-1_B1243030.py
@@ -2,10 +2,10 @@ from rich import print
 
 if __name__ == "__main__":
     nums = input("輸入一串用 , 分隔的數字：").split(",")
-    print("總數：", len(nums))
-    print("總和：", sum(map(int, nums)))
-    print("最小值與最大值：", min(map(int, nums)), max(map(int, nums)))
-    print("找出某個數字出現了幾次：", nums.count(input("請輸入要查詢的數字：")))
-    print("找出某個數字第一次出現的位置：", nums.index(input("請輸入要查詢的數字：")))
-    full_string = ",".join(nums)
-    print(full_string)
+    target_num = input("請輸入要查詢的數字：")
+    buffer = "總數：" + str(len(nums)) + "\n" \
+        + "總和：" + str(sum(map(int, nums))) + "\n" \
+        + "最小值與最大值：" + str(min(map(int, nums))) + ", " + str(max(map(int, nums))) + "\n" \
+        + "找出某個數字出現了幾次：" + str(nums.count(target_num)) + "\n" \
+        + "找出某個數字第一次出現的位置：" + str(nums.index(target_num)) + "\n"
+    print(buffer)
